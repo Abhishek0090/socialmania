261b9134fc83895769c7defdce9b51496a996be9
Abhishek0090 <palabhishek411@gmail.com>
2023-03-13 16:46:56 +0530

    added followers

---
+++ client/src/Data/PostsData.js
@@ -0,0 +1,29 @@
+import postPic1 from '../img/postpic1x.jpg'
+import postPic2 from '../img/postpic2x.jpg'
+import postPic3 from '../img/postpic3x.jpg'
+
+
+export const PostsData = [
+    {
+        img: postPic1,
+        name: 'L Lawliet',
+        desc: "Happy New Year all friends! #2023",
+        likes: 2300,
+        liked: true
+    },
+    {
+        img: postPic2,
+        name: 'Broly',
+        desc: "Party time :)",
+        likes: 2300,
+        liked: false
+
+    },
+    {
+        img:postPic3,
+        name: "Sukuna",
+        desc: "At Archery Festival",
+        likes: 800,
+        liked: false
+    }
+]
