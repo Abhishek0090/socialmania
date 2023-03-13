429046328f0694645f4655e60157a462e7d8e337
Abhishek0090 <palabhishek411@gmail.com>
2023-03-13 11:11:07 +0530

    added profile card and fixing hr line

--- client/src/components/ProfileCard/ProfileCard.jsx
+++ client/src/components/ProfileCard/ProfileCard.jsx
@@ -1,15 +1,39 @@
 import React from 'react'
 import Cover from '../../img/coverx.jpg'
-import Profile from '../../img/profileimgx.png'
+import Profile from '../../img/profileImg.jpg'
 import './ProfileCard.css';
 
 const ProfileCard = () => {
   return (
     <div className='ProfileCard'>
-        <div className="ProfileImages">
-            <img src={Cover} alt="coverimg" />
-            <img src={Profile} alt="profileimg" />
+      <div className="ProfileImages">
+        <img src={Cover} alt="coverimg" />
+        <img src={Profile} alt="profileimg" />
+      </div>
+
+      <div className='ProfileName'>
+        <span>Abhishek Pal</span>
+        <span>Jr Software Engineer</span>
+      </div>
+
+      <div className='followStatus'>
+        <hr />
+        <div>
+          <div className='follow'>
+            <span>6969</span>
+            <span>Followings</span>
+          </div>
+          <div className='vl'></div>
+          <div className='follow'>
+            <span>1</span>
+            <span>Followers</span>
+          </div>
         </div>
+        <hr />
+      </div>
+      <span>
+        My Profile
+      </span>
     </div>
   )
 }
