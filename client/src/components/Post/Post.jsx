e757f0fb5cc19ab20dbff359f564e4bd81ebf4b1
Abhishek0090 <palabhishek411@gmail.com>
2023-03-14 18:41:21 +0530

    added posts and images

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import './Post.css';
+import Comment from "../../img/comment.png";
+import Share from "../../img/share.png";
+import Heart from "../../img/like.png";
+import NotLike from "../../img/notlike.png";
 
-const Post = () => {
-  return (
-    <div>Post</div>
-  )
+const Post = ({data}) => {
+    return (
+        <div className='Post'>
+            <img src={data.img} alt='postimage'/>
+
+            <div className='postReact'>
+                <img src={data.postPic1} alt='post1'/>
+                <img src={data.postPic2} alt='post2'/>
+                <img src={data.postPic3} alt='post3'/>
+            </div>
+        </div>
+    )
 }
 
 export default Post
