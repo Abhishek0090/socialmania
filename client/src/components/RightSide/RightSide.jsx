5506c6ca9f5ce5b52e66fd6ce2a8de352e859edd
Abhishek Pal <palabhishek411@gmail.com>
2023-03-15 00:10:23 +0530

    completed posts section and  on trending section

---
+++ client/src/components/RightSide/RightSide.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./RightSide.css";
+import Home from "../../img/home.png";
+import Noti from "../../img/noti.png";
+import Comment from "../../img/comment.png";
+import { UilSetting } from "@iconscout/react-unicons";
+
+const RightSide = () => {
+  return (
+    <div className="RightSide">
+      <div className="navIcons">
+        <img src={Home} alt="homeIcon" />
+        <UilSetting />
+        <img src={Noti} alt="Notification" />
+        <img src={Comment} alt="Comment" />
+      </div>
+
+      
+    </div>
+  );
+};
+
+export default RightSide;
