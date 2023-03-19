b766ce12317a4657f91844e80b292b2c6a99fca5
Abhishek Pal <palabhishek411@gmail.com>
2023-03-19 16:56:39 +0530

    Register added api

--- server/routes/AuthRoute.js
+++ server/routes/AuthRoute.js
@@ -1,11 +1,10 @@
 import express from "express";
+import {registerUser} from '../controllers/AuthController.js'
 
 const router = express.Router();
 
 
-router.get('/',async (req,res)=>{
-    res.send("Hi bro")
-});
+router.post('/register',registerUser);
 
 
 
