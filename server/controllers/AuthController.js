13cb6d371dc5fb89c5fc47bbf3e4d065987464a0
Abhishek Pal <palabhishek411@gmail.com>
2023-03-19 21:42:57 +0530

    login api check

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,16 +1,48 @@
 import UserModel from "../models/UserModel.js";
+import bcrypt from "bcrypt";
 
 //Register
 export const registerUser = async (req, res) => {
-
   const { username, password, firstname, lastname } = req.body;
 
-  const newUser = new UserModel({ username, password, firstname, lastname });
+  const salt = await bcrypt.genSalt(10);
+  const hashedPass = await bcrypt.hash(password, salt);
+
+  const newUser = new UserModel({
+    username,
+    password: hashedPass,
+    firstname,
+    lastname,
+  });
 
   try {
     await newUser.save();
     res.status(200).json(newUser);
   } catch (error) {
-    res.status(500).json({message : error});
+    res.status(500).json({ message: error });
+  }
+};
+
+//Login
+
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await UserModel.findOne({ username: username });
+
+    if (user) {
+      const validity = await bcrypt.compare(password, user.password);
+
+      if (!validity) {
+        res.status(400).json("wrong password");
+      } else {
+        res.status(201).json("User Login");
+      }
+    } else {
+      res.status(404).json("User Doesnt Exist");
+    }
+  } catch (error) {
+    res.status(500).json({ message: error });
   }
 };
