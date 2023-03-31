94c8b7c480ecc889988748372320f465e51fabb8
Abhishek0090 <palabhishek411@gmail.com>
2023-03-31 11:26:16 +0530

    added chat feature initialisation

--- client/src/App.js
+++ client/src/App.js
@@ -34,6 +34,10 @@ function App() {
           element={user ? <Profile /> : <Navigate to="../auth" />}
         />
 
+        <Route
+          path="/chat"
+          element={user ? <Chat /> : <Navigate to="../auth" />}
+        />
       </Routes>
     </div>
   );
