cd39f6da48cccd2506d58ec322a885f3c0a8e794
Abhishek0090 <palabhishek411@gmail.com>
2023-04-01 12:01:20 +0530

    fixed middleware issue and jwt

--- server/middleware/AuthMiddleware.js
+++ server/middleware/AuthMiddleware.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 
 dotenv.config();
-const secret = process.env.JWTKEY;
+const secret = process.env.JWT_SECRET;
 const AuthMiddleWare = async (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
